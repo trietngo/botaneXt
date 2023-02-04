1d00554ab0f94ca76688fc69254dcf6b8866a6c4
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-02-03 17:59:51 -0800

    add pic selection

--- pages/filter.py
+++ pages/filter.py
@@ -1,18 +1,23 @@
 from service.locateService import *
 import streamlit as st
+from streamlit_image_select import image_select
+
+st.set_page_config(
+    page_title="plant gallery", page_icon="🍃",
+    initial_sidebar_state="collapsed")
+st.markdown("# Plant Gallery")
 
 # Store the initial value of widgets in session state
 st.session_state.placeholder = "Boston"
 st.session_state.disabled = False
 
 # form
-# form = st.form(key="form_settings")
 col1, col2, col3 = st.columns([2, 1, 2])
 with col1:
     st.caption("Preference")
     purpose = st.selectbox("Select your purpose",
                            ('Food (vegetables & fruit)',
-                            'Decoration (plants & flowers)',
+                            'Decoration (data & flowers)',
                             'Both'),
                            label_visibility='collapsed')
 
@@ -39,4 +44,21 @@ with col3:
         placeholder=st.session_state.placeholder,
         value=st.session_state.placeholder
     )
+
+example_image_pattern = "data/plant_pics/{}.png"
+EXAMPLES = ["tomato", "potato", "onion", "strawberry"]
+example_image_fp = [
+    example_image_pattern.format(name.lower()) for name in EXAMPLES[:4]
+]
+index_selected = image_select(
+    "",
+    images=example_image_fp,
+    captions=EXAMPLES[:4],
+    index=0,
+    return_value="index",
+)
+if index_selected != st.session_state["previous_example_index"]:
+    name_selected = EXAMPLES[index_selected]
+    st.session_state.update(EXAMPLES[name_selected].copy())
+    st.session_state["previous_example_index"] = index_selected
 
