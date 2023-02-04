2855ee924f08f999a7f646afa771a140ed895cc7
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-02-03 16:39:31 -0800

    modified filter layout

--- pages/filter.py
+++ pages/filter.py
@@ -6,8 +6,8 @@ st.session_state.placeholder = "Boston"
 st.session_state.disabled = False
 
 # form
-form = st.form(key="form_settings")
-col1, col2, col3 = form.columns([2, 1, 2])
+# form = st.form(key="form_settings")
+col1, col2, col3 = st.columns([2, 1, 2])
 with col1:
     st.caption("Preference")
     purpose = st.selectbox("Select your purpose",
@@ -15,6 +15,9 @@ with col1:
                             'Decoration (plants & flowers)',
                             'Both'),
                            label_visibility='collapsed')
+
+    submitted = st.button("search")
+
 with col2:
     st.caption("Allergy")
     allergy = st.selectbox("Select",
@@ -24,17 +27,16 @@ with col2:
                            label_visibility='collapsed')
 
 with col3:
-    # address = col3.text_input("City")
     st.session_state.disabled = st.checkbox("locate me")
     if st.session_state.disabled:
         ip = get_ip()
         address, _, _ = get_location(ip)
         st.session_state.placeholder = address
-
     address = col3.text_input(
-        "Enter some text 👇",
+        "City",
+        label_visibility="collapsed",
         disabled=st.session_state.disabled,
         placeholder=st.session_state.placeholder,
+        value=st.session_state.placeholder
     )
 
-form.form_submit_button(label="Submit")
