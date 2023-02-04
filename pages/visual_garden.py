30037325496704fb1aa594d0685b015679521f78
hua2980 <95843009+hua2980@users.noreply.github.com>
2023-02-04 13:18:32 -0800

    add back button

--- pages/visual_garden.py
+++ pages/visual_garden.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(
+    page_title="visualize garden", page_icon="🍃",
+    initial_sidebar_state="collapsed",
+    layout="centered"
+)
 
 greetings = st.title('_Flora / Edible Selected: Roses_')
 col1, col2, col3 = st.columns([1, 1, 1])
@@ -7,7 +14,7 @@ with col1:
     beforeImage = col1.image("https://st2.depositphotos.com/1010683/9547/i/950/depositphotos_95478192-stock-photo-beautiful-empty-garden.jpg", width= 300)
 
 
-with col2:  
+with col3:
     after = col3.header("_After_")
     afterImage = col3.image("https://www.youredm.com/wp-content/uploads/2021/05/nurture-flipped.jpg", width= 300)
 
@@ -24,3 +31,13 @@ st.write("Use our virtual try-on feature to get a glimpse of what your garden co
 
 st.file_uploader("_Upload your garden here_")
 st.write("_Privacy Notice: By using our app, you understand and agree that any images provided will only be used for the purpose of generating a virtual representation of your garden using our AI model. We will not save any images or use them for any other purpose. Your privacy and security are of utmost importance to us_")
+
+col4, col5 = st.columns(2)
+
+with col4:
+    if st.button("Back 🔙"):
+        switch_page("filter")
+
+with col5:
+    if st.button("Soil Test 🔬"):
+        switch_page("userForm")
